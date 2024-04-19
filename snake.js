5350550a5cc38c41b8155165999f4101b26900b8
Coco-Bd <146067674+Coco-Bd@users.noreply.github.com>
2024-04-19 17:14:54 +0200

    js snake base

--- snake.js
+++ snake.js
@@ -1,61 +1,33 @@
-const container = document.getElementById("game");
-const canva = document.createElement("div");
-const border = document.createElement("div");
-container.appendChild(border);
-border.appendChild(canva);
+document.addEventListener("DOMContentLoaded", (event) => {
+  const snake = { x: 10, y: 10 };
 
-/*------------------------style------------------------*/
-border.style.aspectRatio = "4/3";
-border.style.height = "100%";
-border.style.border = "5px solid black";
-border.style.boxShadow = "inner 0 0 10px black";
-canva.style.backgroundColor = "grey";
-canva.style.display = "grid";
-canva.style.width = "100%";
-canva.style.height = "100%";
-canva.style.gridTemplateColumns = "repeat(20, 1fr)";
-canva.style.gridTemplateRows = "repeat(20, 1fr)";
+  const gameboard = document.getElementById("game-board");
+  const snakeElement = document.createElement("div");
+  snakeElement.className = "snake";
+  const foodElement = document.createElement("div");
+  foodElement.className = "food";
 
-/*------------------------snake------------------------*/
+  function game() {
+    drawFood();
+    drawSnake();
+    console.log("Snake.js loaded!");
+  }
 
-const snake = [(x = 10), (y = 10)];
-let direction = "right";
-let score = 0;
-let speed = 200;
-let gameover = false;
+  function drawSnake() {
+    snakeElement.style.gridRowStart = snake.y;
+    snakeElement.style.gridColumnStart = snake.x;
+    snakeElement.style.backgroundColor = "var(--cal-poly-green)";
+    snakeElement.style.filter = "opacity(100%)";
+    gameboard.appendChild(snakeElement);
+  }
 
-function draw() {
-  canva.innerHTML = ""; //reset board
-  drawSnake();
-}
+  function drawFood() {
+    foodElement.style.gridRowStart = 1; //Math.floor(Math.random() * 20);
+    foodElement.style.gridColumnStart = 1; //Math.floor(Math.random() * 20);
 
-function drawSnake() {
-  snake.forEach(() => {
-    const snakeElement = document.createElement("div");
-    snakeElement.style.gridColumn = snake[0];
-    snakeElement.style.gridRow = snake[1];
-    snakeElement.style.backgroundColor = "black";
-    canva.appendChild(snakeElement);
-  });
-}
+    foodElement.style.filter = "opacity(100%)";
+    gameboard.appendChild(foodElement);
+  }
 
-function move() {
-  addEventListener("keydown", (direction) => {
-    switch (direction) {
-      case "up":
-        snake[1]--;
-        break;
-      case "down":
-        snake[1]++;
-        break;
-      case "left":
-        snake[0]--;
-        break;
-      case "right":
-        snake[0]++;
-        break;
-    }
-  });
-}
-
-drawSnake();
+  game();
+});
